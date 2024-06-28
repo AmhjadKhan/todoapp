de7adfda86973ad9248c384928facc049ee4d3b2
Amhjadkhan <amjadpogramer@gmail.com>
2024-06-29 00:33:24 +0600

    this is sheadcn ui model design comit

--- todofrontend/src/components/todo/TodoCard.tsx
+++ todofrontend/src/components/todo/TodoCard.tsx
@@ -1,15 +1,16 @@
+import { Button } from "../ui/button"
 
 const TodoCard = () => {
   return (
     <div>
-      <div className="bg-white rounded-md flex justify-between items-center p-3">
+      <div className="bg-white rounded-md flex justify-between items-center p-3 border">
         <input type="checkbox" name="" id="" />
         <p className="font-semibold">Todo Title</p>
         <p>Time</p>
         <p>Descraption</p>
         <div className="space-x-5">
-        <button>del</button>
-        <button>edit</button>
+        <Button className="bg-red-700">Del</Button>
+        <Button className="bg-[#5C53FE]">edit</Button>
         </div>
       </div>
     </div>
--- todofrontend/src/components/todo/TodoContainer.tsx
+++ todofrontend/src/components/todo/TodoContainer.tsx
@@ -1,11 +1,15 @@
 import TodoCard from "./TodoCard";
+import { Button } from "../ui/button";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div className="flex justify-between mb-5 "></div>
+      <div className="flex justify-between mb-5 ">
+        <Button className="bg-primary-gradient">Add todo</Button>
+        <Button className="bg-primary-gradient">Filter</Button>
+      </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
-        <div className="bg-red-300 p-5 w-full h-full rounded-lg space-y-3">
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
           <TodoCard />
           <TodoCard />
           <TodoCard />
